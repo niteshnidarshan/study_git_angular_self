b83bef847b10b9af4a912d1123e0eaec382e8dda
NITESH NIDARSHAN <niteshnidarshan@192.168.1.4>
2020-04-28 14:08:23 +0530

    Navigation with hyperlink

--- angular_project_navigation/PAGE-NAVIGATION/src/app/app-routing.module.ts
+++ angular_project_navigation/PAGE-NAVIGATION/src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { OthersComponent } from './others/others.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home", component:HomeComponent},
+  {path:"others", component:OthersComponent},
+  {path:"contact", component:ContactComponent},
+  {path:"about", component:AboutComponent},
+  {path:"login", component:LoginComponent},
+  {path:"", component:LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular_project_navigation/PAGE-NAVIGATION/src/app/app.module.ts
+++ angular_project_navigation/PAGE-NAVIGATION/src/app/app.module.ts
@@ -1,15 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
+import { OthersComponent } from './others/others.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent,
+    LoginComponent,
+    OthersComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
