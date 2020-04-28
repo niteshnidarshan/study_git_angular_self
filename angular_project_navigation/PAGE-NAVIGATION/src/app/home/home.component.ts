b9f909ecf8221547f0a1fe1b89697f555c84d84e
NITESH NIDARSHAN <niteshnidarshan@192.168.1.4>
2020-04-28 22:54:20 +0530

    design changes & *ngFor used

--- angular_project_navigation/PAGE-NAVIGATION/src/app/home/home.component.ts
+++ angular_project_navigation/PAGE-NAVIGATION/src/app/home/home.component.ts
@@ -7,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   
+  imageList:any[] = [
+    {image:"menhi_digital311.JPG"},
+    {image:"menhi_digital312.JPG"},
+    {image:"menhi_digital313.JPG"},
+    {image:"menhi_digital314.JPG"},
+    {image:"menhi_digital315.JPG"},
+    {image:"menhi_digital320.JPG"},
+    {image:"menhi_digital321.JPG"},
+    {image:"menhi_digital322.JPG"},
+    {image:"menhi_digital324.JPG"},
+    {image:"menhi_digital325.JPG"},
+    {image:"menhi_digital327.JPG"},
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
